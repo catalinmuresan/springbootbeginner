442ec06b04b9d21f012bb8f813d62311a62b1a31
catalinm <muresan_catalin85@yahoo.com>
2019-11-08 14:30:29 +0200

    - initial commit

---
+++ src/main/java/com/catalinm/springbootbeginner/SpringBootBeginnerApplication.java
@@ -0,0 +1,12 @@
+package com.catalinm.springbootbeginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBeginnerApplication.class, args);
+    }
+}
